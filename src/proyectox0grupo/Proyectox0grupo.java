1b0b8de49f69ac8ad2106133a3a4e4e91cada366
RonyUnitec <rony.zuniga@unitec.edu>
2019-02-01 10:38:53 -0600

    imprimiendo marca en el tablero

--- src/proyectox0grupo/Proyectox0grupo.java
+++ src/proyectox0grupo/Proyectox0grupo.java
@@ -18,6 +18,9 @@ public class Proyectox0grupo {
         int marca1,marca2;
         String A1="1String",A2="2",A3="3",B1="4",B2="5",B3="6",C1="7",C2="8",C3="9";
         boolean turnos = true;
+        boolean VA1=false,VA2=false,VA3=false,
+                VB1=false,VB2=false,VB3=false,
+                VC1=false,VC2=false,VC3=false;
         
         //________________________
         
@@ -58,7 +61,183 @@ public class Proyectox0grupo {
                marca2 = leer.nextInt(); 
            
            }
+        if (marca1 > 0 && marca1 <10 ){
+             
+               switch(marca1){
+               
+               case(1):
+                   if(VA1 ==false){
+                   A1 = String.valueOf(simbolo1);
+                   VA1 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(2):
+                   if(VA2 ==false){
+                   A2 = String.valueOf(simbolo1);
+                   VA2 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(3):
+                   if(VA3 ==false){
+                   A3 = String.valueOf(simbolo1);
+                   VA3 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(4):
+                   if(VB1 ==false){
+                   B1= String.valueOf(simbolo1);
+                   VB1 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(5):
+                   if(VB2 ==false){
+                   B2= String.valueOf(simbolo1);
+                   VB2 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(6):
+                   if(VB3 ==false){
+                   B3= String.valueOf(simbolo1);
+                   VB3 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(7):
+                   if(VC1 ==false){
+                   C1= String.valueOf(simbolo1);
+                   VC1 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(8):
+                   if(VC2 ==false){
+                   C2= String.valueOf(simbolo1);
+                   VC2 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;
+               
+               case(9):
+                   if(VC3 ==false){
+                   C3= String.valueOf(simbolo1);
+                   VC3 = true;
+                   turnos = !turnos;
+                   }else{System.out.println("Posicion ocupada o numero invalido");}
+               break;   
+               }
+
+           }      
+           else if(marca2  >0 && marca2 <10 ){
+         
+            
+              
+                    switch(marca2){
+                    case(1):
+                        if(VA1 ==false){
+                        A1 = String.valueOf(simbolo2);
+                        VA1 = true;
+                        turnos = !turnos;
+                     
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(2):
+                        if(VA2 ==false){
+                        A2 = String.valueOf(simbolo2);
+                        VA2 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(3):
+                        if(VA3 ==false){
+                        A3 = String.valueOf(simbolo2);
+                        VA3 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(4):
+                        if(VB1 ==false){
+                        B1= String.valueOf(simbolo2);
+                        VB1 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(5):
+                        if(VB2 ==false){
+                        B2= String.valueOf(simbolo2);
+                        VB2 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(6):
+                        if(VB3 ==false){
+                        B3= String.valueOf(simbolo2);
+                        VB3 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(7):
+                        if(VC1 ==false){
+                        C1= String.valueOf(simbolo2);
+                        VC1 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(8):
+                        if(VC2 ==false){
+                        C2= String.valueOf(simbolo2);
+                        VC2 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+
+                    case(9):
+                        if(VC3 ==false){
+                        C3= String.valueOf(simbolo2);
+                        VC3 = true;
+                        turnos = !turnos;
+                        }else{System.out.println("Posicion ocupada o numero invalido");}
+                    break;
+                    
+                
+                    }
+                    
+                
+
+
+        }else{System.out.println("POSICION INVALIDA!!");} 
         
+            System.out.println("______________________________");
+            System.out.println("||      ||         ||        ||");
+            System.out.println("||  "+A1+"   ||    "+A2+"    ||    "+A3+"   ||");
+            System.out.println("||      ||         ||        ||");
+            System.out.println("______________________________");
+            System.out.println("||      ||         ||        ||");
+            System.out.println("||  "+B1+"   ||    "+B2+"    ||    "+B3+"   ||");
+            System.out.println("||      ||         ||        ||");
+            System.out.println("______________________________");
+            System.out.println("||      ||         ||        ||"); 
+            System.out.println("||  "+C1+"   ||    "+C2+"    ||    "+C3+"   ||");
+            System.out.println("||      ||         ||        ||");
+            System.out.println("______________________________");
     }
     
 }
